a692889ccb8e6ac2ef25df240951d8ca75c61564
santiarco92 <santiago_arco@hotmail.com>
2024-09-09 13:53:53 -0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from "react";
 
 function Header () {
 
-    return <header className="header"> <h1>Santiago Arroyave</h1> 
+    return <header className="header"> 
+    <h1>Santiago Arroyave</h1> 
     <p>Bilingual Project Manager / SCRUM Master</p>
     </header>
 }
