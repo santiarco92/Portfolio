b7325422ae5b03f6b9f935a785cb35cbc419f2d4
santiarco92 <santiago_arco@hotmail.com>
2024-09-09 15:49:29 -0500

    skillsection ready

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,19 +1,60 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import softSkills, {techSkills} from "../info/skills"
+
+
+
+
 
 
 function Education() {
 
-    return <div>
-        <div className="about-me">
-            <h2>About Me</h2>
-            <p>Bilingual and dynamic IT Project Manager and Certified Scrum Master with over 6 years of experience successfully managing diverse projects across diferent sectors. 
-                Adept at leading cross-functional teams, overseeing complex workflows, and consistently delivering results on time and within budget. 
-                Highly skilled in Agile methodologies and stakeholder communication, with a proven ability to transition teams from traditional methods to Scrum, 
-                significantly boosting efficiency and collaboration. Passionate about continuous learning, currently expanding technical expertise in web development. 
-                Recognized for optimizing processes, improving team performance, and consistently exceeding client expectations in international environments.</p>
-            </div> 
-            <div className="skills">
 
+    function createSkill (skill){
+
+        return <li>
+                <p>{skill.skill}</p>
+                <Box sx={{ width: 100 }}>
+                <Slider 
+                    key={skill.id}
+                    defaultValue={skill.level} 
+                    step={1} 
+                    marks min={1} 
+                    max={5} disabled 
+                    sx={{
+                        '& .MuiSlider-thumb': {
+                            width: 10, 
+                            height: 10, 
+                        } 
+                    }}/>
+                </Box>
+
+            </li>
+
+    }
+
+
+
+    return <div>
+            <div className="skills">
+                <div className="softskills">
+                    <h3>Skills</h3>
+                    <div className="s-skills-container">
+                        <ul>
+                           {softSkills.map(createSkill)}
+                        </ul>
+                    </div>
+                </div>
+                <div className="technical-skills">
+                <h3>Technical Skills</h3>
+                    <div className="s-skills-container">
+                        <ul>
+                            {techSkills.map(createSkill)}
+                        </ul>
+                    </div>
+
+                </div>
             </div>
         <div className="education">
             <h5>Field</h5>
---
+++ src/info/skills.jsx
@@ -0,0 +1,104 @@
+const softSkills= [
+    {
+        id: 1,
+        skill:"SCRUM",
+        level: "4"
+    },
+    {
+        id: 2,
+        skill:"Communication",
+        level: "5"
+    },
+    {
+        id: 3,
+        skill:"Leadership",
+        level: "5"
+    },
+    {
+        id: 4,
+        skill:"Teamwork",
+        level: "5"
+    },
+    {
+        id: 5,
+        skill:"Organized",
+        level: "5"
+    },
+    {
+        id: 6,
+        skill:"Project Planning",
+        level: "5"
+    },
+    {
+        id: 7,
+        skill:"Management 3.0",
+        level: "5"
+    },
+    {
+        id: 8,
+        skill:"Attention to details",
+        level: "5"
+    },
+    {
+        id: 9,
+        skill:"Clickup",
+        level: "4"
+    },
+]
+
+
+const techSkills = [ 
+    {
+        id: 1,
+        skill:"HTML 5",
+        level: "2"
+    },
+    {
+        id: 2,
+        skill:"JavaScript",
+        level: "2"
+    },
+    {
+        id: 3,
+        skill:"Node.jsL",
+        level: "1"
+    },
+    {
+        id: 4,
+        skill:"CSS",
+        level: "2"
+    },
+    {
+        id: 5,
+        skill:"Express.js",
+        level: "1"
+    },
+    {
+        id: 6,
+        skill:"Axios",
+        level: "1"
+    },
+    {
+        id: 7,
+        skill:"SQL",
+        level: "1"
+    },
+    {
+        id: 8,
+        skill:"Postgres",
+        level: "1"
+    },
+    {
+        id: 9,
+        skill:"React.js",
+        level: "2"
+    },
+    {
+        id: 10,
+        skill:"Bootstrap",
+        level: "2"
+    },
+]
+
+export default softSkills;
+export {techSkills};
